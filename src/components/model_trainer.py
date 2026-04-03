b5f90ddc8fd5aab24fda75122ab61297d83f2ff9
daksh1293 <dakshkhurana1611@gmail.com>
2026-04-03 18:20:51 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,8 +1,6 @@
-import sys
-import os
+import os,sys
 from dataclasses import dataclass
-import numpy as np
-import pandas as pd
+
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -18,19 +16,21 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
+
 from src.utils import save_object,evaluate_models
 
-@dataclass
-class ModeltrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+    
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config=ModeltrainerConfig()
-
+        self.model_trainer_config=ModelTrainerConfig()
+        
     def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Splitting training and test input data")
+            logging.info('Split training and test input data')
             X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
@@ -38,39 +38,85 @@ class ModelTrainer:
                 test_array[:,-1]
             )
             models={
-                "Random forest": RandomForestRegressor(),
-                "Decision tree": DecisionTreeRegressor(),
-                "Gradient boosting": GradientBoostingRegressor(),
-                "Linear regression": LinearRegression(),
-                "K-Neighbors classifier": KNeighborsRegressor(),
-                "XGBclassifier": XGBRegressor(),
-                "CatBoostClassifier": CatBoostRegressor(verbose=False),
-                "Adaboost classifier": AdaBoostRegressor()
+                "Random Forest":RandomForestRegressor(),
+                "Decision Tree":DecisionTreeRegressor(),
+                "Gradient Boosting":GradientBoostingRegressor(),
+                "Linear Regression":LinearRegression(),
+                "K-Neighbour Regressor":KNeighborsRegressor(),
+                "XGBRegressor":XGBRegressor(),
+                "CatBoosting Regressor":CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor":AdaBoostRegressor()
             }
-
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
-
-            best_model_score= max(sorted(model_report.values()))
-
-            best_model_name= list(model_report.keys())[
+         
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbour Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    # 'weights':['uniform','distance'],
+                    # 'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    # 'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                                models=models,param=params)
+            
+            ## To get best model score from dict
+            best_model_score=max(sorted(model_report.values()))
+         
+            ## To get best model name from dict
+            best_model_name=list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
-            best_model = models[best_model_name]
-
+            
+            best_model=models[best_model_name]
+            
+            # print(best_model_name,best_model_score)
+            
             if best_model_score<0.6:
-                raise CustomException("No best model found")
-            logging.info(f"Best model found on train and test data")
+                raise CustomException("No best model found",sys)
+            logging.info('Best model found on both training and testing dataset')
             
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
-
+            
             predicted=best_model.predict(X_test)
-            r2_square=r2_score(y_test,predicted)
-            return r2_square
-
-
+            score = r2_score(y_test, predicted)
+            
+            return score
         except Exception as e:
             raise CustomException(e,sys)
