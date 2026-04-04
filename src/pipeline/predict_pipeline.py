32305b65c94b62928b0b6c7013a663619913a1d4
daksh1293 <dakshkhurana1611@gmail.com>
2026-04-04 19:12:31 +0530

    debugging

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import os
 
 from src.exception import CustomException
 from src.utils import load_object
@@ -11,14 +12,15 @@ class PredictPipeline:
     
     def predict(self,features):
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join("artifacts","preprocessor.pkl")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             
             return preds
+       
         except Exception as e:
             raise CustomException(e,sys)
         
